e5abb0aabe075919bce32b3f605c0eef117f62f5
Mark Heckler <mark.heckler@gmail.com>
2023-03-23 11:32:46 -0500

    Initial commit after rehome

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,19 @@
 variable "AZ_LOCATION" {
   description = "The Azure location where all resources in this example should be created"
-  default     = "westeurope"
+  default     = "eastus"
 }
 
 variable "AZ_RESOURCE_GROUP" {
   description = "The resource group"
+  default     = "mkheck-function-resource-group-2"
 }
 
 variable "AZ_FUNCTION_NAME_APP" {
   description = "The name of the application running functions"
+  default = "spring-native-image"
 }
 
 variable "AZ_STORAGE_NAME" {
   description = "The name of the storage account"
+  default     = "mkheck1function2storage2"
 }
